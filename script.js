a3c458f17ac8ae714cd8d91bac7ee64c8fa92965
Jhanikiraman <jhanikiramanthenneti@gmail.com>
2026-02-15 15:05:24 +0530

    Update script.js
    json modified

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 function showMessage() {
-  document.getElementById("dynamicText").innerHTML =
-    "🚀 Website successfully deployed using AWS CloudFormation & CodePipeline!";
+    alert("🚀 Successfully deployed via AWS CodePipeline!");
 }
+
+function submitForm(event) {
+    event.preventDefault();
+    document.getElementById("formMessage").innerHTML =
+        "✅ Message submitted successfully!";
+}
+
+// Scroll Reveal Animation
+window.addEventListener("scroll", function () {
+    let reveals = document.querySelectorAll(".reveal");
+
+    reveals.forEach(function (element) {
+        let windowHeight = window.innerHeight;
+        let elementTop = element.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            element.classList.add("active");
+        }
+    });
+});
